6cba481216555a7cf6c16bde7963841b6ff680c6
epavlenko <epavlenko@devexperts.com>
2015-08-19 18:06:20 +0300

    Add external options

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 var fs = require('fs');
 var path = require('path');
 var postcss = require('postcss');
-var chalk = require('chalk');
 
-module.exports = postcss.plugin('postcss-regexp', function() {
+var message = 'Regexp matched with';
+var regexp;
+
+module.exports = postcss.plugin('postcss-regexp', function(options) {
 	return function(css, result) {
+		if (!options.regexp) {
+			result.messages.push({
+				type: 'error',
+				plugin: 'postcss-regexp',
+				text:'No regexp provided'
+			});
+			return;
+		}
+		message = options.message ? options.message : message;
+		regexp = options.regexp;
+
 		css.eachDecl(function(decl) {
 			if (decl.value) {
 				processDecl(decl, result);
@@ -15,8 +28,7 @@ module.exports = postcss.plugin('postcss-regexp', function() {
 
 function processDecl(decl, result) {
 	var value = decl.value;
-	if (value.search(new RegExp('^\\$.*')) === 0) {
-		result.warn('unresolved variable ' + chalk.red(value) +
-			' on line ' + decl.source.start.line);
+	if (value.search(regexp) === 0) {
+		result.warn(message  +' ' + value + ' on line ' + decl.source.start.line);
 	}
 }
