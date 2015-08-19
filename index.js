33f1686f5a28ac12759586c249fb703d14bce4b3
epavlenko <epavlenko@devexperts.com>
2015-08-19 18:23:25 +0300

    Refactor a bit

--- index.js
+++ index.js
@@ -2,20 +2,16 @@ var fs = require('fs');
 var path = require('path');
 var postcss = require('postcss');
 
-var message = 'Regexp matched with';
+var messageText = 'Regexp matched with';
 var regexp;
 
-module.exports = postcss.plugin('postcss-regexp', function(options) {
+module.exports = postcss.plugin('postcss-regexp', function(options, t) {
 	return function(css, result) {
 		if (!options.regexp) {
-			result.messages.push({
-				type: 'error',
-				plugin: 'postcss-regexp',
-				text:'No regexp provided'
-			});
+			throwOptionsError(result);
 			return;
 		}
-		message = options.message ? options.message : message;
+		messageText = options.message ? options.message : messageText;
 		regexp = options.regexp;
 
 		css.eachDecl(function(decl) {
@@ -25,10 +21,20 @@ module.exports = postcss.plugin('postcss-regexp', function(options) {
 		})
 	}
 });
+function throwOptionsError(result){
+	result.messages.push({
+		type: 'error',
+		plugin: 'postcss-regexp',
+		text: 'No regexp provided'
+	});
+}
 
 function processDecl(decl, result) {
 	var value = decl.value;
+
 	if (value.search(regexp) === 0) {
-		result.warn(message  +' ' + value + ' on line ' + decl.source.start.line);
+		var message = messageText + ' ' + value
+			+ ' on line ' + decl.source.start.line;
+		result.warn(message);
 	}
 }
